5eb61d89c02d0535e02684db0cadc4a9be07ac7c
Koinworks-mbp <koinworks-mbp@Koinworks-mbps-MacBook-Pro-2.local>
2022-12-01 10:16:17 +0700

    change env

--- config/postgres/postgres.go
+++ config/postgres/postgres.go
@@ -16,11 +16,11 @@ var (
 )
 
 func Connect() {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	host := os.Getenv("PGHOST")
+	port := os.Getenv("PGPORT")
+	user := os.Getenv("PGUSER")
+	password := os.Getenv("PGPASSWORD")
+	dbname := os.Getenv("PGDATABASE")
 
 	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
 	dsn := config
